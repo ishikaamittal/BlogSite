bb5c39aba9af16b1e2314faf04ccdbd276584fd0
Ishika <mittalishika1801@gmail.com>
2022-02-01 19:26:47 +0530

    templating

--- Blog/forms.py
+++ Blog/forms.py
@@ -1,3 +1,4 @@
+from tkinter import Widget
 from django import forms
 from .models import PostBlog
 
@@ -6,3 +7,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = PostBlog
         fields = ["title","content","category"]
+
+    title= forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Title', 'class': 'form-control'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Article content', 'class': 'form-control'}))
+    category = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'category', 'class': 'form-control'}))
