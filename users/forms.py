f92eeee661c1b2e82cca90dd053b6b2d21d24b16
Ishika <mittalishika1801@gmail.com>
2022-02-03 13:42:08 +0530

    updates

--- users/forms.py
+++ users/forms.py
@@ -22,16 +22,6 @@ class UserRegisterForm(UserCreationForm):
     password1 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'placeholder': 'password', 'class': 'form-control' ,'autocomplete':'off'}))
     password2 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'placeholder': 'password', 'class': 'form-control','autocomplete':'off'}))
 
-    # def clean_password2(self):
-    #     password1 = self.cleaned_data.get("password1")
-    #     password2 = self.cleaned_data.get("password2")
-    #     if password1 and password2 and password1 != password2:
-    #         raise ValidationError(
-    #             self.error_messages['password_mismatch'],
-    #             code='password_mismatch',
-    #         )
-    #     return password2
-        
     def clean(self):
        email = self.cleaned_data.get('email')
        if User.objects.filter(email=email).exists():
@@ -44,12 +34,18 @@ class UserRegisterForm(UserCreationForm):
 
 
 class updateUserForm(forms.ModelForm):
-    email = forms.EmailField()
     class Meta:
         model = User
-        fields = ['email','username']
+        fields = ['first_name','last_name','email','username']
+
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control','autocomplete':'off'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'class': 'form-control','autocomplete':'off'}))
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control','autocomplete':'off'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'E-mail ID', 'class': 'form-control','autocomplete':'off'}))
+
 
 class updateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['img']
+    
