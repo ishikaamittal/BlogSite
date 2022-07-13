67151fac717e749bc1d6b71e52a658d298e8a9a0
Ishika <mittalishika1801@gmail.com>
2022-07-13 16:08:46 +0530

    tkinter remove

--- Blog/views.py
+++ Blog/views.py
@@ -8,7 +8,7 @@ from .models import Blog
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import BlogForm, updateBlogForm
+from .forms import updateBlogForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
